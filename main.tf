3c10e0940a8f34950d364a4e491188d79eb40868
mrigank810 <mriganksaxena.24@gmail.com>
2024-01-05 17:57:53 +0530

    Updated code for VPC deployment

--- main.tf
+++ main.tf
@@ -37,8 +37,63 @@ resource "aws_iam_role" "lambda_execution_role" {
   })
 }
 
+resource "aws_iam_policy" "lambda_eni_policy" {
+  name        = "LambdaENIManagementPolicy"
+  description = "IAM policy for Lambda to manage ENIs for VPC access"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ec2:CreateNetworkInterface",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DeleteNetworkInterface",
+          "ec2:AssignPrivateIpAddresses",
+          "ec2:UnassignPrivateIpAddresses"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_eni_policy_attachment" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = aws_iam_policy.lambda_eni_policy.arn
+}
+
 resource "aws_iam_role_policy_attachment" "lambda_read_only" {
   role       = aws_iam_role.lambda_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+resource "aws_iam_role_policy_attachment" "lambda_CWlogs_policy" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+}
+resource "aws_security_group" "lambda_sg" {
+  name        = "lambda-security-group"
+  description = "Security group for Lambda functions"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Lambda Security Group"
+  }
+}
+
+resource "aws_vpc_endpoint" "s3_endpoint" {
+  vpc_id       = module.vpc.vpc_id
+  service_name = "com.amazonaws.us-east-1.s3"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = module.vpc.public_route_table_ids
 }
 
--- output.tf
+++ output.tf
@@ -2,6 +2,21 @@ output "VPC_ID" {
     value = module.vpc.vpc_id
 }
 
-output "execution_role_arn" {
+output "lambda_execution_role_arn" {
     value = aws_iam_role.lambda_execution_role.arn
+}
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnets
+  description = "The IDs of the public subnets"
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnets
+  description = "The IDs of the private subnets"
+}
+
+output "security_group_id" {
+  value = aws_security_group.lambda_sg.id
+  description = "The ID of the Lambda Security Group"
 }
